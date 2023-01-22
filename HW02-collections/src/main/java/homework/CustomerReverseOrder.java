5cd6525a05e294167898038d700a0f8f25b7fc23
Константин Ш dinamexoft <kosshitikhin@gmail.com>
2023-01-23 02:02:00 +0300

    HW02-collections практика работы с коллекциями, структурами данных

---
+++ HW02-collections/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,19 @@
+package homework;
+
+
+import java.util.Stack;
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final Stack<Customer> customerStack = new Stack<>();
+
+    public void add(Customer customer) {
+        customerStack.add(customer);
+    }
+
+    public Customer take() {
+        return customerStack.pop();
+    }
+}
