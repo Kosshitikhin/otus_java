d0656077ca5853b115ca14c6724bfaa1d11d1d04
Константин Ш dinamexoft <kosshitikhin@gmail.com>
2023-03-23 00:12:27 +0300

    refactoring

--- HW07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ HW07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,7 +2,9 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -13,12 +15,34 @@ public class HistoryListener implements Listener, HistoryReader {
 
     @Override
     public void onUpdated(Message msg) {
-        var msgCopy = msg.createMessage();
+        var msgCopy = copyMessage(msg);
         messageMap.put(msgCopy.getId(), msgCopy);
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
         return Optional.ofNullable(messageMap.get(id));
+    }
+
+    private Message copyMessage(Message message) {
+        var field13 = new ObjectForMessage();
+        field13.setData(new ArrayList<>());
+        field13.getData().addAll(message.getField13().getData());
+
+        return new Message.Builder(message.getId())
+                .field1(message.getField1())
+                .field2(message.getField2())
+                .field3(message.getField3())
+                .field4(message.getField4())
+                .field5(message.getField5())
+                .field6(message.getField6())
+                .field7(message.getField7())
+                .field8(message.getField8())
+                .field9(message.getField9())
+                .field10(message.getField10())
+                .field11(message.getField11())
+                .field12(message.getField12())
+                .field13(field13)
+                .build();
     }
 }
--- HW08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ HW08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -4,10 +4,12 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 
 public class ResourcesFileLoader implements Loader {
 
@@ -21,8 +23,11 @@ public class ResourcesFileLoader implements Loader {
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
 
-        try (var reader = new InputStreamReader(new FileInputStream(fileName))){
-            return gson.fromJson(reader, new TypeToken<List<Measurement>>(){}.getType());
+        try (var is = getClass().getClassLoader().getResourceAsStream(fileName);
+             var isReader = new InputStreamReader(Objects.requireNonNull(is), StandardCharsets.UTF_8);
+             var buffReader = new BufferedReader(isReader)
+        ){
+            return gson.fromJson(buffReader, new TypeToken<List<Measurement>>(){}.getType());
         } catch (IOException e) {
             throw new FileProcessException(e);
         }
