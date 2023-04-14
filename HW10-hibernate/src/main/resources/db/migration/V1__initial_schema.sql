093eedb8f8579ff905c9cd332ce27dce8fae0360
Константин Ш dinamexoft <kosshitikhin@gmail.com>
2023-04-14 20:25:40 +0300

    HW10-hibernate

---
+++ HW10-hibernate/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,25 @@
+create sequence client_SEQ start with 1 increment by 1;
+
+create table client
+(
+    id   bigint not null primary key,
+    name varchar(50),
+    address_id bigint
+);
+
+create sequence phone_SEQ start with 1 increment by 1;
+
+create table phone
+(
+    id        bigint not null primary key,
+    number    varchar(50),
+    client_id bigint
+);
+
+create sequence address_SEQ start with 1 increment by 1;
+
+create table address
+(
+    id     bigint not null primary key,
+    street varchar(50)
+);
